3c1b886ebce4cf22fb438181bd83ba3afbd8813b
Manojkumar0718 <manojkumar2002july@gmail.com>
2026-06-17 22:18:50 +0530

    basics and strings

---
+++ basics.py
@@ -0,0 +1,45 @@
+# print statement in Python is used to display output to the user. 
+# In this case, we are using the print function to display a message about learning Python basics.
+print("I am learning Python basics!")
+# The print function can also be used to display the value of variables. 
+# Here, we are defining a variable called 'language' and assigning it the value 'Python'. Then we use the print function to display the value of the variable.
+language = "Python" 
+print(language)
+
+#variables
+#variables in Python are used to store data that can be used later in the program.
+# In this example, we are defining a variable called 'age' and assigning it the value 25. 
+# We can then use this variable in our program to perform operations or display its value.
+age = 25
+print(age)
+# We can also perform operations on variables.
+# For example, we can add two numbers together and store the result in a new variable called 'sum'.
+num1 = 10
+num2 = 20
+sum = num1 + num2
+print(sum)
+#rules for naming variables in Python:
+# 1. Variable names must start with a letter (a-z, A-Z) or an underscore (_).
+# 2. Variable names can only contain letters, numbers, and underscores.
+# 3. Variable names cannot be the same as Python keywords (e.g., if, else, while).
+# 4. Variable names are case-sensitive (e.g., age and Age are different variables).
+#primitive data types
+# In Python, there are several primitive data types that are used to represent different kinds of data.
+# The most common primitive data types include:
+# 1. int: used to represent whole numbers (e.g., 1, 2, 3)
+# 2. float: used to represent decimal numbers (e.g., 3.14, 2.5)
+# 3. str: used to represent text (e.g., "Hello, World!")
+# 4. bool: used to represent boolean values (True or False)
+# Here are some examples of using these primitive data types:
+# Integer
+my_int = 42
+print(my_int)
+# Float
+my_float = 3.14
+print(my_float)
+# String
+my_string = "Hello, World!"
+print(my_string)
+# Boolean
+my_bool = True
+print(my_bool)
